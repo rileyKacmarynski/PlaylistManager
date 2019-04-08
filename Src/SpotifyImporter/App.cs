c3d50f03fa2ae8b33e50b740a2af931d792a5ff1
Riley Kacmarynski <rkacmarynski@omnitech-inc.com>
2019-04-08 15:41:26 -0500

    Set up configuration and DI for importer project.

---
+++ Src/SpotifyImporter/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpotifyImporter
+{
+    public class App
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Src/SpotifyImporter/Program.cs
+++ Src/SpotifyImporter/Program.cs
@@ -1,18 +1,56 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Abstractions;
+using Microsoft.Extensions.Logging;
 
 namespace SpotifyImporter
 {
     class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
         static void Main(string[] args)
         {
-            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
             if (string.IsNullOrWhiteSpace(env))
             {
                 env = "Development";
             }
 
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, true)
+                .AddJsonFile($"appsettings{env}.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            if (env == "Development")
+            {
+                builder.AddUserSecrets<Program>();
+            }
+            
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddOptions();
+            serviceCollection.Configure<AppSettings>(builder.Build().GetSection("AppSettings"));
+
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddLogging(configure =>
+            {
+                configure.AddConsole();
+                configure.AddDebug();
+            });
+
+            serviceCollection.AddTransient<App>();
         }
     }
 }
