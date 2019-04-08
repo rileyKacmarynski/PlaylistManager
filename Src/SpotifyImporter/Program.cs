9595dd6ca03b5bf4e56d1536b879f0620cc159f7
Riley Kacmarynski <rkacmarynski@omnitech-inc.com>
2019-04-08 14:23:02 -0500

    Create initial project

---
+++ Src/SpotifyImporter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SpotifyImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (string.IsNullOrWhiteSpace(env))
+            {
+                env = "Development";
+            }
+
+        }
+    }
+}
